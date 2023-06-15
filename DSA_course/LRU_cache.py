749e523cadc9860e71122e4672753c01e5fe713d
Will <willchesson@gmail.com>
2023-06-15 16:54:28 -0400

    advanced stack

--- DSA_course/LRU_cache.py
+++ DSA_course/LRU_cache.py
@@ -8,12 +8,79 @@
 #also have a map that keeps track of each node's value (4: 4node, 3: 3node) for O(1) access
 
 class LRUCache:
-    def __init__(self, ):
-        pass
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.nodeMap = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     #check if key exists, if not returns - 1
-    def get(key):
-        pass
+    #if key exists, move person to front of list and return the value
+    def get(self, key):
+        currentNode = self.nodeMap[key]
+        if not currentNode: return -1
 
-    def put(key, value):
-        pass
+        #move to front of list
+        self.delete(currentNode)
+        self.insert_at_front(currentNode)
+
+        return currentNode.value
+
+
+    def put(self, key, value):
+        #if customer exists, don't have to worry about capacity. Just need to move to front and update their value
+        if self.nodeMap[key]:
+            currentNode = self.nodeMap[key]
+            self.delete(currentNode)
+            self.insert_at_front(currentNode)
+
+            #update value
+            currentNode.value = value
+            self.nodeMap[key] = currentNode
+        else:
+            #if the customer doesn't exist, have to see if exceed capacity with this new customer
+            if self.capacity == self.nodeMap.size():
+                #delete last person in list
+                lastNode = self.tail.prev
+                #delete from list
+                self.delete(lastNode)
+                #delete from nodeMap
+                self.nodeMap.delete(lastNode.key)
+
+            #create new node and add new customer/node
+            #add them to start of list
+            newNode = Node(key, value)
+            self.insert_at_front(newNode)
+            #insert into nodeMap
+            self.nodeMap[key] = newNode
+
+
+    #inserts a node at front of list
+    #call when new customer and when an existing customer does anything
+    def insert_at_front(self, currentNode):
+        #head.next should point to this node
+        #this node needs to point what previous head.next was pointing to
+        prevFirstNode = self.head.next
+        prevFirstNode.prev = currentNode
+        self.head.next = currentNode
+        currentNode.prev = self.head
+
+    #called when move an existing node to front
+    #or when exceed our capacity and to remove very last node
+    def delete(currentNode):
+        nextNode = currentNode.next
+        prevNode = currentNode.prev
+
+        #make nextNode and prevNode point to each other
+        nextNode.prev = prevNode
+        prevNode.next = nextNode
+
+
+class Node:
+    def __init__(self, key, value):
+        self.prev = None
+        self.next = None
+        self.key = key
+        self.value = value
---
+++ DSA_course/advanced_stack.py
@@ -0,0 +1,62 @@
+#leetcode 227 - basic calculator II
+#integer division
+#the difficulty is PEMDAS
+#we don't know when are going to reach operator or space or how many characters comprise each number
+
+import math
+
+def calculate(s):
+    #num keeps track of the num ahead of the operator
+    num = ""
+    #sign will be the previous sign
+    sign = None
+    stack = []
+    i=0
+
+    while i < len(s):
+        char = s[i]
+        if char == " ": continue
+
+        #if char is number, then add to num
+        if char.isdigit():
+            num += char
+        else:
+            #if reach this block, we know that we hit an operator
+            num = int(num)
+            print("else block", num)
+
+            if not sign or sign == "+":
+                stack.append(num)
+                print("+", stack)
+            elif sign == "-":
+                stack.append(-num)
+                print("-", stack)
+            elif sign == "*":
+                multipliedNum = stack.pop() * num
+                stack.append(multipliedNum)
+                print("*", stack)
+            elif sign == "/":
+                dividedNum = stack.pop() // num
+                stack.append(dividedNum)
+                print("/", stack)
+
+            sign = char
+            print("sign", sign)
+            num = ""
+
+        i+=1
+        print("i", i)
+
+    print("stack before while", stack)
+    #at the end of the loop will have result of any multiplication or division in the stack
+    #if the sign was "-", then push the negative of that number in the stack
+    #therefore can simply sum all the remaining numbers in the stack
+    while(len(stack) > 1):
+        stack.append(stack.pop() + stack.pop())
+
+
+    return stack.pop()
+
+
+
+print(calculate("3+2*2"))
